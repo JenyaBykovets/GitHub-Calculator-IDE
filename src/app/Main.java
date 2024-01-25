c6a19f5e4a76dff43fbea17ced3769d45debbdf5
Jeka <jenyabikovec@gmail.com>
2024-01-25 16:13:05 +0200

    dev commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,14 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("My git app.Super");
+    Scanner scanner = new Scanner(System.in);
+    double sc = scanner.nextDouble();
+
     }
-}
+
+    }
+
